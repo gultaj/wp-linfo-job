f83084699c1b9ba0e174050f9736eeadccc6e52d
gultaj_home <yaschick.mail@gmail.com>
2014-11-22 03:10:37 +0300

    add text when nothing found.

--- public/partials/archive-job_vacancy.php
+++ public/partials/archive-job_vacancy.php
@@ -8,13 +8,17 @@
             <div class="content-header row"> <?php Wp_Linfo_Job_Public::title( $object ) ?> </div>
             <div class="job__aside_links"><?= Wp_Linfo_Job_Public::get_archive_link('resume') ?></div>
             <div class="job__flash"><?= Wp_Linfo_Job_Public::flashmessages() ?></div>
+        <?php if (!($vacancies = Wp_Linfo_Job_Public::get_vacancies())) : ?>
+        	<h3>Ничего не найдено.</h3>
+        	<a class="primary" href="<?= home_url('/'.$object->rewrite['slug'].'?new' ); ?>">Добавте свою вакансию</a>
+        <?php else : ?>
             <div class="vacancy__list">
                 <div class="vacancy__list_header">
                     <div class="vacancy__list_name">Вакансия</div>   
                     <div class="vacancy__list_company">Организация</div>
                     <div class="vacancy__list_salary">Оплата</div>    
                 </div>
-                <?php foreach (Wp_Linfo_Job_Public::get_vacancies() as $vacancy) : ?>
+                <?php foreach ($vacancies as $vacancy) : ?>
                 <a href="<?= get_permalink( $vacancy->ID ) ?>" class="vacancy__item">
                     <div class="vacancy__item_name"><?= $vacancy->post_title ?></div>   
                     <div class="vacancy__item_company"><?= $vacancy->company ?></div>
@@ -23,6 +27,7 @@
                 <?php endforeach; ?>
             </div>
             <div class="job__pagination"> <?php lidainfo_paging_nav() ?> </div>
+        <?php endif; ?>
         </div>
     </div>
 </div>
